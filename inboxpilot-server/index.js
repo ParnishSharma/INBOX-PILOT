9ae1217040468e884de88a476c442e49e7220efb
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2025-06-20 13:41:08 +0530

    Update index.js

--- inboxpilot-server/index.js
+++ inboxpilot-server/index.js
@@ -40,10 +40,7 @@ app.use(session({
   },
 }));
 
-mongoose.connect(process.env.MONGO_URI , {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log(' MongoDB connected');
 
